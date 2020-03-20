561abdc01035c081fd5ec1e29bc47a757513cc60
Acidham <acidham@gmail.com>
2020-03-20 23:39:47 +0100

    fix delete all in a directory

---
+++ src/empty.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import os
+import sys
+
+from Alfred import Tools
+
+HOME = os.path.expanduser('~')
+
+query = Tools.getArgv(1)
+
+root = "{0}/{1}".format(HOME, query)
+
+for it in os.listdir(root):
+    if it != ".DS_Store" and it != "Icon\r":
+        it_path = os.path.join(root, it)
+        if os.path.isdir(it_path):
+            os.removedirs(it_path)
+        elif os.path.islink(it_path):
+            os.unlink(it_path)
+        else:
+            os.remove(it_path)
+sys.stdout.write(root)
