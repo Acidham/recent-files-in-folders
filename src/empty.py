c31d676e14df69e5a5aa4046478c7c793f3fa0b3
Acidham <acidham@gmail.com>
2020-03-20 23:43:08 +0100

    fix remove all items in dir

--- src/empty.py
+++ src/empty.py
@@ -4,6 +4,16 @@ import sys
 
 from Alfred import Tools
 
+
+def remove(p):
+    if os.path.isdir(it_path):
+        os.removedirs(it_path)
+    elif os.path.islink(it_path):
+        os.unlink(it_path)
+    else:
+        os.remove(it_path)
+
+
 HOME = os.path.expanduser('~')
 
 query = Tools.getArgv(1)
@@ -13,10 +23,5 @@ root = "{0}/{1}".format(HOME, query)
 for it in os.listdir(root):
     if it != ".DS_Store" and it != "Icon\r":
         it_path = os.path.join(root, it)
-        if os.path.isdir(it_path):
-            os.removedirs(it_path)
-        elif os.path.islink(it_path):
-            os.unlink(it_path)
-        else:
-            os.remove(it_path)
+        remove(it_path)
 sys.stdout.write(root)
